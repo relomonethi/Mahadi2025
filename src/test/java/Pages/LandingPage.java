99fa9eb13c17d6280a5dfe0e641e185b5a35a8cb
Refilwe007 <123360859+Refilwe007@users.noreply.github.com>
2025-03-22 21:43:38 +0200

    Input username and password on login screen

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,24 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class LandingPage {
+
+    WebDriver driver;
+
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+        public void verifyProductText() {
+            String ProductText = driver.findElement(By.xpath("//*[@id='header_container']/div[2]/span")).getText();
+            Assert.assertEquals(ProductText, "Products");
+        }
+
+        public void addToCart(){
+            WebElement addToCart = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+            addToCart.click();
+        }
 }
